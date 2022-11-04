a2a6d9792f86780ed991d6e6b44d8e14b0a2b065
MichaelG48 <michael_glz48@outlook.es>
2022-11-04 17:14:37 -0600

    New menu bar

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MenuApp from './template/MenuApp'
+import MenuApp2 from './template/MenuApp'
 import Footer from './template/Footer'
 import GaleryArticulos from './index/GaleryArticulos'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MenuApp />
+    <MenuApp2 />
     <GaleryArticulos />
     <Footer />
   </React.StrictMode>
